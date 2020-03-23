4793a14936a128ebf32782215469ed17be322cbe
Bhavik Gosrani <bhavik4748@gmail.com>
2020-03-22 22:57:05 -0400

    breaking reducers into sub reducers part for better managment

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,7 +1,6 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 const initialState = {
     counter: 0,
-    results: []
 }
 
 const reducer = (state = initialState, action) => {
@@ -26,17 +25,7 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter - action.val
             }
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: state.counter })
-            }
-        case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId)
-            return {
-                ...state,
-                results: updatedArray
-            }
+        
     }
     return state;
 };
